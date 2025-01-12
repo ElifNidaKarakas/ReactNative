f0bcc0065fb16c11a6acd748e3dd81efc28df625
ElifNidaKarakas <elifnidakarakas@gmail.com>
2025-01-12 17:07:06 +0300

    database üzerinden veri çekme

--- Restoran/restoran/src/ResaultShowScreen.js
+++ Restoran/restoran/src/ResaultShowScreen.js
@@ -1,13 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View , Image} from "react-native";
+import React, { useEffect, useState } from "react";
+import yelp from "../api/yelp";
 
-//id bilgilerine göre çekme ve resme tıklama 
-export default function ResaultShowScreen() {
+
+//id bilgilerine göre çekme ve resme tıklama
+export default function ResaultShowScreen({ route }) {
+  const [result, setResults] = useState(null);
+  const id = route.params.id; //id değerini çekeriz
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResults(response.data);
+  };
+
+  useEffect(() => {
+    //sayfanın sürekli istek atmaması için yaparız
+    getResult(id);
+  }, []);
+  if (!result) {
+    return null;
+  }
   return (
     <View>
-      <Text>ResaultShowScreen</Text>
+      <Text>{result.name}</Text>
+      <Text>{result.phone}</Text>
+      <FlatList
+        data={result.photos}
+        renderItem={({ item }) => {
+          return <Image style={styles.image}
+           source={{ uri: item }} />;
+        }}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image:{
+        width:300, 
+        height:150,
+        marginLeft:10,
+        marginBottom:10,
+    }
+});
