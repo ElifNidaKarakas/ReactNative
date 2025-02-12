d20eec3328a02479b95758418c5df10f69d2f000
ElifNidaKarakas <elifnidakarakas@gmail.com>
2025-02-12 20:52:18 +0300

    Modal added

---
+++ Kurslar/kurslarim/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+
+export default function HomeScreen({ navigation }) {
+  //ANA SAYFADIR
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>ANA EKRAN</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
